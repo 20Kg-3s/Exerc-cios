f719768806a3719ce02cd9b5425c564ba503c410
VinteKls_Dev <81206911+20Kg-3s@users.noreply.github.com>
2021-06-24 14:04:49 -0300

    finalizado exercicio 4

--- src/com/company/exercicio4.java
+++ src/com/company/exercicio4.java
@@ -8,6 +8,11 @@ public class exercicio4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite suas 4 notas bimestrais");
         int [] notas = new int[4];
-
+        int soma =0;
+        for (int i=0;i< notas.length;i++){
+            notas[i] = sc.nextInt();
+            soma += notas[i];
+        }
+        System.out.println("A média das notas é: "+(soma/ notas.length));
     }
 }
